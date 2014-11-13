304f037d43b5e2e6bb6173a41ffce673a75ddf98
stefan <stefan9106@gmail.com>
2014-11-13 22:30:42 +0200

    second commit

--- classes/Candidate.php
+++ classes/Candidate.php
@@ -6,21 +6,22 @@
  * @author stefan
  */
 class Candidate {
-    
+
     private $mysql;
-    
+
     public function __construct() {
-        $this->mysqli = mysqli_connect('localhost', 'root', '1111', 'CayetanoTask');  
-    }    
+        $this->mysqli = mysqli_connect('localhost', 'root', '1111', 'CayetanoTask');
+    }
 
     public function index() {
         $response = new stdClass();
-        
+
         $jobs = $this->mysqli->query('SELECT * FROM candidates');
         $allCandidates = array();
         while ($row = $jobs->fetch_assoc()) {
             $allCandidates[] = $row;
         }
+        $response->status = 'ok';
         $response->data = $allCandidates;
 
         echo json_encode($response);
@@ -31,12 +32,15 @@ class Candidate {
         $result = $this->mysqli->query('SELECT * FROM candidates WHERE id =' . $id);
 
         if (!$result) {
+            $response->status = 'error';
             $response->message = 'Error';
         } else {
             $candidate = $result->fetch_row();
             if (!$candidate) {
+                $response->status = 'error';
                 $response->message = 'Error';
             } else {
+                $response->status = 'ok';
                 $response->data = $candidate;
             }
         }
@@ -49,13 +53,15 @@ class Candidate {
         $result = $this->mysqli->query("SELECT * FROM candidates WHERE id LIKE '%" . $id . "%'");
 
         if (!$result) {
+            $response->status = 'error';
             $response->message = 'Error';
         } else {
             $allCandidates = array();
             while ($row = $result->fetch_assoc()) {
                 $allCandidates[] = $row;
             }
-            $response->data = $allCandidates;          
+            $response->status = 'ok';
+            $response->data = $allCandidates;
         }
 
         echo json_encode($response);
--- classes/Job.php
+++ classes/Job.php
@@ -1,53 +1,50 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Job
  *
  * @author stefan
  */
-class Job {    
+class Job {
+
     private $mysql;
-    
+
     public function __construct() {
-        $this->mysqli = mysqli_connect('localhost', 'root', '1111', 'CayetanoTask');  
+        $this->mysqli = mysqli_connect('localhost', 'root', '1111', 'CayetanoTask');
     }
 
     public function index() {
         $response = new stdClass();
-        
-           
+
         $jobs = $this->mysqli->query('SELECT * FROM jobs');
-//        $jobs = mysqli_query($this->connection,'SELECT * FROM jobs');
         $allJobs = array();
         while ($row = $jobs->fetch_assoc()) {
             $allJobs[] = $row;
         }
-        $response->data = $allJobs;        
-        
+        $response->status = 'ok';
+        $response->data = $allJobs;
+
         echo json_encode($response);
     }
 
     public function show($id) {
         $response = new stdClass();
-        
+
         $result = $this->mysqli->query('SELECT * FROM jobs WHERE id =' . $id);
-        
+
         if (!$result) {
+            $response->status = 'error';
             $response->message = 'Error';
         } else {
             $job = $result->fetch_row();
             if (!$job) {
+                $response->status = 'error';
                 $response->message = 'Error';
             } else {
+                $response->status = 'ok';
                 $response->data = $job;
             }
-        }        
+        }
 
         echo json_encode($response);
     }
